5d3ad5add25c67cfc1f38dbb1980bff2fabda133
rakeshr <rakeshrajendran123@gmail.com>
2025-11-13 22:43:24 +0530

    Initial Commit

---
+++ Day 3/Multiple Ifs/task.py
@@ -0,0 +1,23 @@
+#print on the screen welcome message
+print("Welcome to the rollercoaster ride")
+height= int(input("Enter you height ? "))
+bill =0
+if height >= 120 :
+     print("You can go ahead and ride")
+     age= int(input("Enter your age ? "))
+     if age <=12:
+         print("It will cost $5")
+         bill=5
+     elif age > 12 and age <=18:
+         print("It will cost $7")
+         bill=7
+     else:
+         print("It will cost $12")
+         bill=12
+#Ask user if they want photo to be taken add $3 to their overall bill
+     photo= input("Do you want photos ? for Yes type Y and for No type N: ")
+     if photo == "Y":
+         bill= bill +3
+     print(f"Your final bill is ${bill}")
+else:
+    print("You can't ride because you need to grow up bit")

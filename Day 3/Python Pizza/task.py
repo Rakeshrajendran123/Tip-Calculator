5fb74e1ce47e15274b6d68d9ef5f5bdc927e3bed
Rakesh Rajendran <36667982+Rakeshrajendran123@users.noreply.github.com>
2025-11-14 23:58:10 +0530

    Fix indentation and formatting in task.py

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -5,14 +5,14 @@ pepperoni=input("Do you want Pepperoni for medium or large Pizza(Y or N)? : ")
 cheese = input("Do you want to add extra cheese(Y or N )?: ")
 
 #Initialize Pizzacost
-pizza_cost=0
+pizza_cost = 0
 
 #Pizza cost based on size
 
 if pizza_size == "S":
     pizza_cost += 15
 elif pizza_size == "M":
-    pizza_cost +=20
+    pizza_cost += 20
 elif pizza_size == "L":
     pizza_cost += 25
 else:
@@ -21,13 +21,13 @@ else:
 #Add pepperoni cost to pizza cost
 if pepperoni == "Y":
      if pizza_size == "S":
-         pizza_cost +=2
+         pizza_cost += 2
      else:
-         pizza_cost +=3
+         pizza_cost += 3
 
 #Add Chesse Cost to pizza cost
 if cheese == "Y":
-        pizza_cost +=1
+        pizza_cost += 1
 
 # Print total cost of Pizza based on choices user made
 print(f"The final bill is ${pizza_cost}")

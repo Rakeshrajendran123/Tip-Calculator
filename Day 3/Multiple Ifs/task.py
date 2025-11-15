5a286dea00d5d8883c12a32ea952177d9837a013
rakeshr <rakeshrajendran123@gmail.com>
2025-11-15 23:16:26 +0530

    Initial Commit

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -11,13 +11,19 @@ if height >= 120 :
      elif age > 12 and age <=18:
          print("It will cost $7")
          bill=7
+     elif age >=45 and age <=55:
+         print("You are free to ride")
+         bill=0
      else:
          print("It will cost $12")
          bill=12
 #Ask user if they want photo to be taken add $3 to their overall bill
      photo= input("Do you want photos ? for Yes type Y and for No type N: ")
      if photo == "Y":
-         bill= bill +3
+         if age >=45 and age <=55:
+          bill=0
+         else:
+             bill+=3
      print(f"Your final bill is ${bill}")
 else:
     print("You can't ride because you need to grow up bit")
